c8e7ccd8946eb33b2f541e7088dc513a145d42e2
JaneGH <jkrytsyna@gmail.com>
2024-04-04 14:04:21 -0700

    created water model

---
+++ lib/model/water_madel.dart
@@ -0,0 +1,26 @@
+class WaterModel {
+  final String? id;
+  final double amount;
+  final DateTime dateTime;
+
+  WaterModel(
+      {this.id,
+      required this.amount,
+      required this.dateTime,
+      required String unit});
+  factory WaterModel.fromJson(Map<String, dynamic> json, String id) {
+    return WaterModel(
+        id: id,
+        amount: json['amount'],
+        dateTime: DateTime.parse(json['dateTime']),
+        unit: json['unit']);
+  }
+
+  // conver water model to json for sending data to firebase
+  Map<String, dynamic> toJson() {
+    return {
+      'amount': amount,
+      'dateTime': DateTime.now(),
+    };
+  }
+}
