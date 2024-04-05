2cf03da703c4149319dfc570ae3ade8cc8ad7251
JaneGH <jkrytsyna@gmail.com>
2024-04-05 12:07:19 -0700

    added saving  firebase

---
+++ lib/bars/individual.dart
@@ -0,0 +1,7 @@
+class IndividualBar {
+  final int x;
+  final double y;
+
+  IndividualBar({required this.x, required this.y});
+  //y amount of water
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:water_intake/data/water_data.dart';
+import 'package:water_intake/model/water_madel.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -14,21 +17,21 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final amountController = TextEditingController();
-  void saveWater(String amount) async {
-    final url = Uri.parse(
-        'https://water-intaker-7e68b-default-rtdb.firebaseio.com/water.json');
-    var response = await http.post(url,
-        headers: {'Content-Type': 'application/json'},
-        body: json.encode({
-          'amount': double.parse(amount),
-          'unit': 'ml',
-          'dateTime': DateTime.now().toString()
-        }));
 
-    if (response.statusCode == 200) {
-      print('Data saved');
-    } else {
-      print('Error saving data');
+  @override
+  void initState() {
+    Provider.of<WaterData>(context, listen: false).getWater();
+    super.initState();
+  }
+
+  void saveWater() async {
+    Provider.of<WaterData>(context, listen: false).addWater(WaterModel(
+        amount: double.parse(amountController.text.toString()),
+        dateTime: DateTime.now(),
+        unit: 'ml'));
+
+    if (!context.mounted) {
+      return;
     }
   }
 
@@ -63,7 +66,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 TextButton(
                   onPressed: () {
                     //save data to db
-                    saveWater(amountController.text);
+                    saveWater();
+                    Navigator.of(context).pop();
                   },
                   child: const Text('Save'),
                 ),
@@ -73,19 +77,29 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 4,
-        centerTitle: true,
-        actions: const [
-          Icon(Icons.map), // Remove unnecessary onPressed: null
-        ],
-        title: const Text('Water'),
-      ),
-      backgroundColor: Theme.of(context).colorScheme.background,
-      floatingActionButton: FloatingActionButton(
-        onPressed: addWater,
-        child: const Icon(Icons.add),
+    return Consumer<WaterData>(
+      builder: (context, value, hild) => Scaffold(
+        appBar: AppBar(
+          elevation: 4,
+          centerTitle: true,
+          actions: const [
+            Icon(Icons.map),
+          ],
+          title: const Text('Water'),
+        ),
+        body: ListView.builder(
+            itemCount: value.waterDataList.length,
+            itemBuilder: (context, index) {
+              final waterModel = value.waterDataList[index];
+              return ListTile(
+                title: Text(waterModel.amount.toString()),
+              );
+            }),
+        backgroundColor: Theme.of(context).colorScheme.background,
+        floatingActionButton: FloatingActionButton(
+          onPressed: addWater,
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
